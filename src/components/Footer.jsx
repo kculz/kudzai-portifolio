b9e41ada2feefd50df20572ba1dc65911d375dda
kculz <munyamakudzai095@gmail.com>
2025-04-13 15:01:53 +0200

    CHANGED: contact details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -86,7 +86,7 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
                 <a href="tel:+263775123456" className="hover:text-primary transition-colors">
-                  +263 775 123 456
+                  +263 771 392 149
                 </a>
               </li>
               <li className="flex items-start text-gray-300">
@@ -94,7 +94,7 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 </svg>
-                <span>Harare, Zimbabwe</span>
+                <span>Mutare, Zimbabwe</span>
               </li>
             </ul>
           </div>
